44c204f14eabea894f819bf530775fa3c95c31da
Tom E Flaherty <teflaherty@utexas.edu>
2025-04-20 21:00:05 -0500

    Add project files.

---
+++ RAA_Int_Module03_Skills/Common/CommandAvailability.cs
@@ -0,0 +1,17 @@
+﻿namespace RAA_Int_Module03_Skills.Common
+{
+    internal class CommandAvailability : IExternalCommandAvailability
+    {
+        public bool IsCommandAvailable(UIApplication applicationData, CategorySet selectedCategories)
+        {
+            bool result = false;
+            UIDocument activeDoc = applicationData.ActiveUIDocument;
+            if (activeDoc != null && activeDoc.Document != null)
+            {
+                result = true;
+            }
+
+            return result;
+        }
+    }
+}
